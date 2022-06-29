2b1ba0cab918642bad8b645034297b6ec776d88b
James940909 <85343934+James940909@users.noreply.github.com>
2022-06-29 19:17:27 +0900

    에러 해결 및 웹 템플릿 적용 테스트 완료

---
+++ ToyProject/src/main/java/controller/mainController.java
@@ -0,0 +1,18 @@
+package controller;
+
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class mainController {
+	
+	@RequestMapping("/index.do")
+	public String selectOneMeal(HttpSession session) {
+		return "index.jsp";
+	}
+
+}
--- ToyProject/src/main/java/model/store/storeVO.java
+++ ToyProject/src/main/java/model/store/storeVO.java
@@ -1,5 +1,48 @@
 package model.store;
 
 public class storeVO {
-
+	private int s_num;
+	private String s_name;
+	private String s_category;
+	private String s_address;
+	private int s_reviewCnt;
+	
+	public int getS_num() {
+		return s_num;
+	}
+	public void setS_num(int s_num) {
+		this.s_num = s_num;
+	}
+	public String getS_name() {
+		return s_name;
+	}
+	public void setS_name(String s_name) {
+		this.s_name = s_name;
+	}
+	public String getS_category() {
+		return s_category;
+	}
+	public void setS_category(String s_category) {
+		this.s_category = s_category;
+	}
+	public String getS_address() {
+		return s_address;
+	}
+	public void setS_address(String s_address) {
+		this.s_address = s_address;
+	}
+	public int getS_reviewCnt() {
+		return s_reviewCnt;
+	}
+	public void setS_reviewCnt(int s_reviewCnt) {
+		this.s_reviewCnt = s_reviewCnt;
+	}
+	
+	@Override
+	public String toString() {
+		return "storeVO [s_num=" + s_num + ", s_name=" + s_name + ", s_category=" + s_category + ", s_address="
+				+ s_address + ", s_reviewCnt=" + s_reviewCnt + "]";
+	}
+	
+	
 }
