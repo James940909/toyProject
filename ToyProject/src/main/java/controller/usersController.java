46c022ecb8fb038919bed06e0c428809e78a9b4a
James940909 <85343934+James940909@users.noreply.github.com>
2022-07-05 18:13:43 +0900

    insertUsers구현중 오류 발생 및 해결중에 있음

--- ToyProject/src/main/java/controller/usersController.java
+++ ToyProject/src/main/java/controller/usersController.java
@@ -2,11 +2,13 @@ package controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import model.users.usersService;
 import model.users.usersVO;
 
+@Controller
 public class usersController {
 	
 	@Autowired
