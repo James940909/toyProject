e3041eb0c6a98adfb0557f5c8ccc7e431dfe3cbd
James940909 <85343934+James940909@users.noreply.github.com>
2022-07-06 19:46:59 +0900

    .do 확장자 오류 잡는중..

--- ToyProject/src/main/java/controller/mainController.java
+++ ToyProject/src/main/java/controller/mainController.java
@@ -1,17 +1,21 @@
 package controller;
 
 
-import javax.servlet.http.HttpSession;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import model.users.usersVO;
 
 
 @Controller
 public class mainController {
 	
-	@RequestMapping("/main.do")
-	public String selectOneMeal(HttpSession session) {
+	@Autowired
+	usersVO usersVO;
+	
+	@RequestMapping(value="/main.do")
+	public String selectOneMeal(usersVO vo) {
 		return "main.jsp";
 	}
 
